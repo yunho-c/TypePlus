7248667132872faa02bcbf307ade0293337bfc3c
Yunho Cho <yhjc2692@gmail.com>
2025-03-17 14:03:40 -0400

    Sorted imports in `main.dart`

--- typeplus_one/lib/main.dart
+++ typeplus_one/lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:typeplus_one/ui/main_screen.dart';
 import 'package:typeplus_one/services/worker.dart';
 import 'package:typeplus_one/services/window.dart';
+import 'package:typeplus_one/themes.dart';
+import 'package:typeplus_one/ui/main_screen.dart';
 import 'package:typeplus_one/ui/side_pane.dart';
 import 'package:typeplus_one/ui/search_bar.dart';
-import 'package:typeplus_one/themes.dart';
 
 void main() async {
   configureWindow();
